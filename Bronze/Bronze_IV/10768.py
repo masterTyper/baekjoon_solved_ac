90862e3b7f99bff4bb9ad2ce3b54df36070c110a
masterTyper <hyunwookim.master@gmail.com>
2022-07-23 13:45:18 +0900

    문제 풀이

---
+++ Bronze/Bronze_IV/10768.py
@@ -0,0 +1,11 @@
+month = int(input())
+day = int(input())
+
+if month < 2:
+    print("Before")
+elif month == 2 and day < 18:
+    print("Before")
+elif month == 2 and day == 18:
+    print("Special")
+else:
+    print("After")
